ba6bd4495168469dcbdfceebc21118d641d76fb1
Ryan <rmfarrell@gmail.com>
2019-03-25 14:51:07 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Collection from './components/Collection';
+import List from './components/List';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/collection">Collection</Link>
+            </li>
+            <li>
+              <Link to="/list">List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/collection" exact component={Collection} />
+        <Route path="/list" exact component={List} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default App;
+export default AppRouter;
