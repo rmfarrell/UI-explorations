56eca2cc4686421c3931bfc270a9a28cf04e2bfb
Ryan <rmfarrell@gmail.com>
2019-03-27 19:46:06 -0400

    MediumListItem

--- src/components/LayoutGenerator.js
+++ src/components/LayoutGenerator.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { teaser } from '../mocks/generator';
+import ListItem from './ListItem';
+import styles from '../styles/LayoutGenerator.module.css';
 
 class LayoutGenerator extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return '';
+    const teasers = [];
+    for (let x = 0; x < 5; x++) {
+      teasers.push(teaser());
+    }
+    return (
+      <article className={[styles.root, 'constrain'].join(' ')}>
+        <div className={styles.group}>
+          <ul className={styles.small}>
+            {teasers.map(t => {
+              return <ListItem data={t} />;
+            })}
+          </ul>
+          <ul className={styles.medium}>
+            {teasers.map(t => {
+              return <ListItem data={t} size={1} />;
+            })}
+          </ul>
+        </div>
+      </article>
+    );
   }
+  componentDidMount() {}
 }
-
 export default LayoutGenerator;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,26 +3,56 @@ import styles from '../styles/ListItem.module.css';
 
 // TODO this could be merged with Card probably
 export default function ListItem({ data = {}, className = '', size = 0 }) {
-  const {
-      title,
-      source,
-      author,
-      date,
-      country,
-      issues,
-      image,
-      type,
-      summary
-    } = data,
-    imgPlaceholder = {
-      background: `${image.color}`,
-      height: '0',
-      paddingBottom: `${image.ratio}%`
-    };
+  console.log(data);
   return (
-    <li className={[styles.root, className].join(' ')}>
-      <h4>{date}</h4>
-      <h3>{title}</h3>
+    <li className={[styles.root, className, styles[`size-${size}`]].join(' ')}>
+      <a href="#">
+        {size === 0 && <SmallTeaser {...data} />}
+        {size === 1 && <MediumTeaser {...data} />}
+      </a>
     </li>
   );
+}
+
+function SmallTeaser({ date = '', title = '' }) {
+  return (
+    <div>
+      <h4>{date}</h4>
+      <h3>{title}</h3>
+    </div>
+  );
+}
+
+function MediumTeaser({
+  date = '',
+  title = '',
+  image = {},
+  summary = '',
+  source = '',
+  author = ''
+}) {
+  const { color = '#000', ratio = 100 } = image;
+  return (
+    <div className={styles.columnContainer}>
+      <div className={styles.imgContainer}>
+        <div style={horizontalImageStyle(color, ratio)} />
+      </div>
+      <div className={styles.textContainer}>
+        <h4>{date}</h4>
+        <h3>{title}</h3>
+        <h5>
+          By {author}, <em>{source}</em>
+        </h5>
+        <p>{summary}</p>
+      </div>
+    </div>
+  );
+}
+
+function horizontalImageStyle(color, ratio) {
+  return {
+    background: `${color}`,
+    height: '0',
+    paddingBottom: `${ratio}%`
+  };
 }
