3b3fe02972cf0bf37cb8e2d1cb82048e5d30e560
Ryan <rmfarrell@gmail.com>
2019-03-27 21:29:20 -0400

    defaults

--- src/components/LayoutGenerator.js
+++ src/components/LayoutGenerator.js
@@ -12,12 +12,12 @@ class LayoutGenerator extends Component {
       <article className={[styles.root, 'constrain'].join(' ')}>
         <div className={styles.group}>
           <ul className={styles.small}>
-            {teasers(12).map(t => {
+            {teasers(6).map(t => {
               return <ListItem data={t} />;
             })}
           </ul>
           <ul className={styles.medium}>
-            {teasers(5).map(t => {
+            {teasers(2).map(t => {
               return <ListItem data={t} size={1} />;
             })}
           </ul>
