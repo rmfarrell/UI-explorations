a7165aacacbbe6c9dc4f07c37e52d47b7b5a949c
Ryan <rmfarrell@gmail.com>
2019-03-28 12:25:33 -0400

    code cleanup

--- src/mocks/generator.js
+++ src/mocks/generator.js
@@ -26,6 +26,7 @@ const lorem = new LoremIpsum({
 function teaser() {
   return {
     id: uuid(),
+    contentType: 'Article',
     title: lorem.generateSentences(1),
     source: capitalize(lorem.generateWords(3)),
     author: capitalize(lorem.generateWords(2)),
@@ -38,30 +39,32 @@ function teaser() {
   };
 }
 
-function summary() {
-  const { title, date, image, source, id } = teaser();
-  return {
-    title,
-    date: date(),
-    image: image(),
-    source,
-    id
-  };
-}
-
-const Article = teaser;
-const articles = teasers;
-
 function SocialMediaItem() {
   return {
+    id: uuid(),
     contentType: 'Social Media Item',
-    source: ['Twitter', 'Facebook', 'Instagram', 'LinkedIn'][randomInt(0, 3)],
+    source: pickRandomFromArray(['Twitter', 'Facebook', 'Instagram']),
     text: '',
     author: '',
     image: image(),
     date: date()
   };
 }
+
+function DeepDive() {
+  return {
+    id: uuid(),
+    title: lorem.generateSentences(2),
+    issues: issues(),
+    summary: lorem.generateSentences(3),
+    image: image(),
+    type: type()
+  };
+}
+
+// aliases
+const Article = teaser;
+const articles = teasers;
 
 // -- Multiples
 function teasers(n = 1) {
@@ -76,11 +79,13 @@ function multiple(n = 0, type = 'Article') {
   const out = [];
   for (let x = 0; x < n; x++) {
     switch (type) {
-      case 'Article':
+      case 'article':
         out.push(Article());
         break;
-      case 'Social':
+      case 'social':
         out.push(SocialMediaItem());
+        break;
+      case 'mixed':
         break;
       default:
         out.push(Article());
@@ -92,29 +97,14 @@ function multiple(n = 0, type = 'Article') {
 
 // -- Field Generators
 
+// For now, this combines Entities & Countries, primaries come first
 function issues() {
-  const times = randomInt(1, 2),
-    out = [];
-  for (let x = 0; x < times; x++) {
-    let topics = TOPICS.filter(t => !out.includes(t));
-    out.push(topics[randomInt(0, topics.length)]);
-    TOPICS.slice();
-  }
-  return out;
-}
-
-function randomInt(min = 0, max = 100) {
-  return Math.floor(Math.random() * Math.floor(max)) + min;
+  const count = randomInt(1, 3);
+  return pickRandomFromArray(TOPICS.slice(0), count);
 }
 
 function type() {
   return TYPES[randomInt(0, TYPES.length)];
-}
-
-function randomColor() {
-  return `rgb(${randomInt(0, 150)}, 
-  ${50}, 
-  ${randomInt(0, 150)})`;
 }
 
 function image() {
@@ -128,7 +118,7 @@ function date() {
   return 'November 31, 2019';
 }
 
-// -- Helpers
+// -- Helpers/utilities
 
 function capitalize(str = '') {
   return str
@@ -139,10 +129,28 @@ function capitalize(str = '') {
     .join(' ');
 }
 
+function randomColor() {
+  return `rgb(${randomInt(0, 150)}, 
+  ${50}, 
+  ${randomInt(0, 150)})`;
+}
+
+function randomInt(min = 0, max = 100) {
+  return Math.floor(Math.random() * Math.floor(max)) + min;
+}
+
+function pickRandomFromArray(arr = [null], n = 1) {
+  const out = [];
+  for (let x = 0; x < n; x++) {
+    let filtered = arr.filter(t => !out.includes(t));
+    out.push(filtered[randomInt(0, filtered.length - 1)]);
+  }
+  return out;
+}
+
 export {
   teaser,
   teasers,
-  summary,
   randomColor,
   TYPES,
   TOPICS,
