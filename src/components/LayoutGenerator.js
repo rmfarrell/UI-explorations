bd3fae01a77f4b53b787daf2295e4f76031ccf8e
Ryan <rmfarrell@gmail.com>
2019-04-02 16:20:35 -0400

    categories numbers

--- src/components/LayoutGenerator.js
+++ src/components/LayoutGenerator.js
@@ -4,6 +4,16 @@ import Carousel from './Carousel';
 import ListItem from './ListItem';
 import styles from '../styles/LayoutGenerator.module.css';
 
+const categories = [
+  'latestDevelopments',
+  'policyDocuments',
+  'analysis',
+  'opinion',
+  'media',
+  'social',
+  'data',
+  'deepDives'
+];
 const mocks = {
   columns: {
     social: socialMediaItems(3),
@@ -15,10 +25,67 @@ const mocks = {
 class LayoutGenerator extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showStatus: true,
+      categories: categories.reduce((acc, cat) => {
+        acc[cat] = {
+          total: 0,
+          images: 0
+        };
+        return acc;
+      }, {})
+    };
   }
+  setArticleQuantity = ({ target }) => {
+    const { name, value } = target,
+      [key, val] = name.split('-'),
+      newVal = Object.assign(this.state.categories[key], { [val]: value });
+    console.log(newVal);
+    this.setState({
+      categories: Object.assign(this.state.categories, {
+        [key]: newVal
+      })
+    });
+  };
   render() {
+    const subject = Object.keys(this.state.categories).map(cat => {
+      return (
+        <div key={cat}>
+          <h4>{cat}</h4>
+          <label htmlFor={`${cat}-total`}>total</label>
+          <input
+            type="number"
+            name={`${cat}-total`}
+            value={this.state.categories[cat].total}
+            onChange={this.setArticleQuantity}
+            min="0"
+          />
+          {this.state.categories[cat].total > 0 && cat !== 'media' && (
+            <span>
+              <label htmlFor={`${cat}-images`}>with images</label>
+              <input
+                type="number"
+                name={`${cat}-images`}
+                value={this.state.categories[cat].images}
+                onChange={this.setArticleQuantity}
+                max={this.state.categories[cat].total}
+                min="0"
+              />
+            </span>
+          )}
+        </div>
+      );
+    });
     return (
       <article className={[styles.root, 'constrain'].join(' ')}>
+        <form>
+          <label htmlFor="showStatus">Show Status</label>
+          <input type="checkbox" checked={this.state.showStatus} />
+          <div className={styles.articlesConfig}>
+            <h3>Articles</h3>
+            {subject}
+          </div>
+        </form>
         <h1>Columns</h1>
         <div className={styles.group}>
           <ul className={styles.medium}>
