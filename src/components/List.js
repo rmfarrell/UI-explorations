1f1c9ac923b670f8e79e7ec061599e0de14ece69
Ryan <rmfarrell@gmail.com>
2019-04-06 19:49:45 -0400

    list refactor

--- src/components/List.js
+++ src/components/List.js
@@ -4,67 +4,128 @@ import styles from '../styles/List.module.css';
 import Carousel from './Carousel';
 import ListItem from './ListItem';
 
-class Collection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      teasers: []
-    };
-  }
-  render() {
-    const {
-      className,
-      children,
-      variant,
-      groupSize,
-      showControls = true,
-      showReadMore = false,
-      perpage = 5
-    } = this.props;
-    return (
-      <aside className={[styles.root, className].join(' ')}>
-        {children && <header>{children}</header>}
-        <Carousel
-          variant={variant}
-          groupSize={groupSize}
-          showControls={showControls && this.total > perpage}
-        >
-          {this.slides.map((collection, idx) => {
-            return (
-              <ul key={idx} className={styles.itemList}>
-                {collection.map(item => {
-                  return <ListItem key={item.id} data={item} />;
-                })}
-              </ul>
-            );
-          })}
-        </Carousel>
-        {showReadMore && (
-          <button onClick={this.props.expand} className={styles.readmore}>
-            Read more ({this.total})
-          </button>
-        )}
-      </aside>
-    );
-  }
-  get total() {
-    return this.props.total || 35;
-  }
-  get slides() {
-    const perpage = this.props.perpage || 5;
-    const out = [],
-      slidesCounts = Math.ceil(this.state.teasers.length / perpage);
-    for (let x = 0; x < slidesCounts; x++) {
-      out.push(this.state.teasers.slice(x, perpage + x));
+export default function List({
+  className = '',
+  children,
+  variant,
+  groupSize = 1,
+  showControls = true,
+  showReadMore = false,
+  perpage = 5,
+  total = 0,
+  expand = false
+}) {
+  //   get slides() {
+  //     const perpage = this.props.perpage || 5;
+  //     const out = [],
+  //       slidesCounts = Math.ceil(this.state.teasers.length / perpage);
+  //     for (let x = 0; x < slidesCounts; x++) {
+  //       out.push(this.state.teasers.slice(x, perpage + x));
+  //     }
+  //     return out;
+  //   }
+  let slide = [],
+    slides = [],
+    slidesCounts = Math.ceil(total / perpage),
+    counter = total;
+  while (counter > 0) {
+    if (slide.length < perpage) {
+      slide.push(teaser());
+    } else {
+      slides.push(slide);
+      slide = [teaser()];
     }
-    return out;
+    console.log(counter);
+    counter--;
   }
-  componentDidMount() {
-    const teasers = [];
-    for (let x = 0; x < this.total; x++) {
-      teasers.push(teaser());
-    }
-    this.setState({ teasers });
-  }
+  slides.push(slide);
+  // for (let x = 0; x < slidesCounts; x++) {
+  //   for (let i = 0; i < perpage; i++) {
+  //     slide.push(teaser());
+  //   }
+  //   slides.push(slide);
+  //   slide = [];
+  // }
+  console.log(slides);
+  return (
+    <aside className={[styles.root, className].join(' ')}>
+      {children && <header>{children}</header>}
+      <Carousel
+        variant={variant}
+        groupSize={groupSize}
+        showControls={showControls && total > perpage * groupSize}
+      >
+        {slides.map((collection, idx) => {
+          return (
+            <ul key={idx} className={styles.itemList}>
+              {collection.map(item => {
+                return <ListItem key={item.id} data={item} />;
+              })}
+            </ul>
+          );
+        })}
+      </Carousel>
+      {showReadMore && (
+        <button onClick={expand} className={styles.readmore}>
+          Read more ({total})
+        </button>
+      )}
+    </aside>
+  );
 }
-export default Collection;
+
+// class Collection extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       teasers: []
+//     };
+//   }
+//   render() {
+//     const  = this.props;
+//     return (
+//       <aside className={[styles.root, className].join(' ')}>
+//         {children && <header>{children}</header>}
+//         <Carousel
+//           variant={variant}
+//           groupSize={groupSize}
+//           showControls={showControls && this.total > perpage}
+//         >
+//           {this.slides.map((collection, idx) => {
+//             return (
+//               <ul key={idx} className={styles.itemList}>
+//                 {collection.map(item => {
+//                   return <ListItem key={item.id} data={item} />;
+//                 })}
+//               </ul>
+//             );
+//           })}
+//         </Carousel>
+//         {showReadMore && (
+//           <button onClick={this.props.expand} className={styles.readmore}>
+//             Read more ({this.total})
+//           </button>
+//         )}
+//       </aside>
+//     );
+//   }
+//   get total() {
+//     return this.props.total || 35;
+//   }
+//   get slides() {
+//     const perpage = this.props.perpage || 5;
+//     const out = [],
+//       slidesCounts = Math.ceil(this.state.teasers.length / perpage);
+//     for (let x = 0; x < slidesCounts; x++) {
+//       out.push(this.state.teasers.slice(x, perpage + x));
+//     }
+//     return out;
+//   }
+//   componentDidMount() {
+//     const teasers = [];
+//     for (let x = 0; x < this.total; x++) {
+//       teasers.push(teaser());
+//     }
+//     this.setState({ teasers });
+//   }
+// }
