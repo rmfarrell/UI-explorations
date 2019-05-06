425be7fb67ded8a62e7ce510aace200d5aa931f4
Ryan <rmfarrell@gmail.com>
2019-05-06 13:32:45 -0400

    modal

--- src/components/CustomArticle.jsx
+++ src/components/CustomArticle.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import styles from '../styles/CustomArticle.module.css';
 import { placeholderImage } from '../lib/helpers';
+import Modal from './Modal.jsx';
 
 export default function(props) {
   const { data } = props;
-  const { title = '', short_description = '' } = data;
+  const { title = '', short_description = '', content = '' } = data;
   console.log(data);
-  return (
-    <article className={styles.root}>
+  function img() {
+    return (
       <div
         className={styles.imgContainer}
         style={placeholderImage('#ccc', 70)}
       />
+    );
+  }
+  return (
+    <article className={styles.root}>
+      {img()}
       <h1>
         <a onClick={openModal}>{title}</a>
       </h1>
       <p>
         {short_description} ... <a onClick={openModal}>READ MORE &raquo;</a>
       </p>
+      <Modal>
+        {img()}
+        <h1>{title}</h1>
+        <p>{content}</p>
+        <p>{content}</p>
+      </Modal>
     </article>
   );
 
