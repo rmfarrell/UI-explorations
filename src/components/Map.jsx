a0befc911d77f721e0c7d82e66eda8c1c1fb23da
Ryan <rmfarrell@gmail.com>
2019-05-07 13:10:54 -0400

    remove zoom feature on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,33 +2,49 @@ import React from 'react';
 import data from '../lib/map';
 import styles from '../styles/Map.module.css';
 import { NavLink } from 'react-router-dom';
+import { COUNTRIES } from '../lib/constants';
 
 export default function(props) {
   const { focus, linkPrefix = '/relationship/' } = props,
-    containerStyles = [styles.container, focus ? styles.focused : ''].join(' ');
-  return (
-    <div className={containerStyles}>
-      <div
-        className={styles.map}
-        style={
-          focus && {
-            transform: `translate(${getFocus()}) scale(5)`,
-            transformOrigin: '0 0'
+    containerStyles = [styles.container, isFocused ? styles.focused : ''].join(
+      ' '
+    ),
+    isFocused = !focus || focus.toLowerCase() === 'europe';
+  function unfocused() {
+    return (
+      <div className={containerStyles}>
+        <div
+          className={styles.map}
+          style={
+            focus && {
+              transform: `translate(${getFocus()}) scale(5)`,
+              transformOrigin: '0 0'
+            }
           }
-        }
-      >
-        {data.map(tiles => {
-          return tiles.map((tile, idx) => {
-            return (
-              <Tile key={idx} data={tile} linkPrefix={linkPrefix}>
-                {tile && tile.country}
-              </Tile>
-            );
-          });
-        })}
+        >
+          {data.map(tiles => {
+            return tiles.map((tile, idx) => {
+              return (
+                <Tile key={idx} data={tile} linkPrefix={linkPrefix}>
+                  {tile && tile.country}
+                </Tile>
+              );
+            });
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  function focused() {
+    return (
+      <div className={styles.focused}>
+        <h1>{COUNTRIES[focus]}</h1>
+      </div>
+    );
+  }
+
+  return isFocused ? unfocused() : focused();
 
   function getFocus() {
     let x, y;
