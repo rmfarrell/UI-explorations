d2a1f39824a7957f86dd64a2bdba3fdf1669c490
Ryan <rmfarrell@gmail.com>
2019-05-08 14:47:51 -0400

    fix collection grid

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import Card from './Card';
 import Masonry from 'react-masonry-component';
+import styles from '../styles/Collection.module.css';
 
 const masonryOptions = {
   // transitionDuration: 0
+  columnWidth: 0
 };
 
 export default React.memo(function(props) {
   const { showType = true, articles = [], className = '' } = props;
   return (
-    <div className={['constrain', className].join(' ')}>
-      <Masonry options={masonryOptions}>
+    <div className={[className].join(' ')}>
+      <Masonry options={masonryOptions} className={[styles.grid].join(' ')}>
         {articles.map(data => {
           return (
-            <Card {...data} key={data.id}>
-              {showType && (
-                <header>
-                  <h3>{data.document_type || data.type}</h3>
-                </header>
-              )}
-            </Card>
+            <div className="grid--item__third">
+              <Card {...data} key={data.id}>
+                {showType && (
+                  <header>
+                    <h3>{data.document_type || data.type}</h3>
+                  </header>
+                )}
+              </Card>
+            </div>
           );
         })}
       </Masonry>
