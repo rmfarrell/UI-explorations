eca0b4daae08cf90bc1854277c30c7cdb6a2a244
Ryan <rmfarrell@gmail.com>
2019-05-21 08:07:12 -0400

    add ArticlesGrid to DeepDive

--- src/pages/DeepDive.jsx
+++ src/pages/DeepDive.jsx
@@ -12,7 +12,7 @@ import Map from '../components/Map.jsx';
 import SingleItem from '../components/SingleItem.jsx';
 import List from '../components/List.jsx';
 import Link from '../components/Link.jsx';
-import ArticleModal from '../components/ArticleModal.jsx';
+import ArticlesGrid from '../components/ArticlesGrid.jsx';
 
 export default React.memo(function(props) {
   const {
@@ -37,38 +37,8 @@ export default React.memo(function(props) {
     collectionData = dereferenceArticles(articles, collection).reduce(
       reduceArticleCollection,
       {}
-    );
-  const tiles = [
-      // Map
-      {
-        cat: 'Map',
-        width: 1
-      },
-      // Custom article
-      Object.assign(custom_article, {
-        cat: 'Custom Article',
-        width: 1,
-        canExpand: true
-      })
-    ]
-      .concat(
-        ...featuredData.map(featured => {
-          return Object.assign(featured, {
-            cat: 'Featured',
-            width: 1,
-            canExpand: true,
-            featured: true
-          });
-        })
-      )
-      .concat(
-        Object.keys(collectionData).map(cat => ({
-          cat,
-          content: collectionData[cat]
-        }))
-      )
-      .map(addWidths),
-    rows = makeGrid(tiles);
+    ),
+    [firstFeatured] = featuredData.splice(0, 1);
 
   return (
     <article>
@@ -78,82 +48,33 @@ export default React.memo(function(props) {
         </h2>
         <h1>{title}</h1>
       </header>
-      {rows.map(({ items, size }, idx) => {
-        return (
-          <div className={classNames('grid', 'constrain')} key={idx}>
-            {items.map(data => {
-              return (
-                <div
-                  key={data.cat}
-                  className={[
-                    tileClassName(size, data.width),
-                    styles.tile
-                  ].join(' ')}
-                >
-                  <Tile
-                    data={data}
-                    category={data.cat}
-                    width={data.width}
-                    row={idx}
-                  />
-                </div>
-              );
-            })}
+      <div className={classNames('grid', 'constrain')}>
+        <div className="grid--item__third">
+          <Map tileClickHandler={id => history.push(`/deep-dives/${id}`)} />
+        </div>
+        <div
+          className={
+            firstFeatured ? 'grid--item__third' : 'grid--item__two-thirds'
+          }
+        >
+          <CustomArticle data={custom_article} />
+        </div>
+        {firstFeatured && (
+          <div className="grid--item__third">
+            <SingleItem
+              data={firstFeatured}
+              className=""
+              size={2}
+              type="article"
+              summaryLines={2}
+              link={`#${firstFeatured.id}`}
+            />
           </div>
-        );
-      })}
-      <ArticleModal />
+        )}
+      </div>
+      <ArticlesGrid featured={featuredData} collection={collectionData} />
     </article>
   );
-
-  /**
-   * Factory funciton for rendering Tiles
-   *
-   */
-  function Tile(props) {
-    const { data, category, width, row = 0 } = props,
-      { content } = data,
-      firstRow = row === 0;
-
-    switch (category) {
-      case 'Custom Article':
-        return <CustomArticle data={custom_article} />;
-      case 'Map':
-        return (
-          <Map
-            // mapFills={id => {
-            //   const count = articleCounts[id] || 0;
-            //   const saturation = 100 * (count / articleCountsMax);
-            //   return `hsl(346, ${saturation}%, 50%)`;
-            // }}
-            tileClickHandler={id => history.push(`/deep-dives/country/${id}`)}
-          />
-        );
-      case 'Featured':
-        return featured(data);
-      default:
-        const perpage = row > 0 ? 4 : 3;
-        return content.length > 1 ? (
-          <List items={content} groupSize={width} perpage={perpage}>
-            <h3>{pluralize(category)}</h3>
-          </List>
-        ) : (
-          featured(content[0])
-        );
-    }
-
-    function featured(data = {}) {
-      return (
-        <SingleItem
-          data={data}
-          className=""
-          size={firstRow ? 2 : width}
-          summaryLines={firstRow ? 0 : 4}
-          type="article"
-        />
-      );
-    }
-  }
 });
 
 function error() {
@@ -164,46 +85,4 @@ function reduceArticleCollection(acc, item) {
   const type = item.document_type || item.type;
   acc[type] = acc[type] ? acc[type].concat(item) : [item];
   return acc;
-}
-
-function addWidths(item) {
-  item.width = item.width || 1;
-  item.canExpand = item.width < 2;
-  return item;
-}
-
-function makeGrid(tiles = []) {
-  const grid = Grid(Row(3));
-  grid.add(tiles);
-  grid.separateFeatured();
-  grid.balance();
-
-  return grid.rows;
-}
-
-function tileClassName(rowSize = 4, width = 1) {
-  const isWide = width > 1;
-  if (rowSize === 3) {
-    return isWide ? 'grid--item__two-thirds' : 'grid--item__third';
-  }
-  if (rowSize === 4) {
-    return isWide ? 'grid--item__half' : 'grid--item__quarter';
-  }
-}
-
-function pluralize(category = '') {
-  switch (category) {
-    case 'Deep Dive':
-      return 'Deep Dives';
-    case 'Policy Document':
-      return 'Policy Documents';
-    case 'Article':
-      return 'Articles';
-    case 'External Resource':
-      return 'External Resources';
-    case 'Social Media Item':
-      return 'Social Media';
-    default:
-      return category;
-  }
 }
