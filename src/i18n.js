1336225a09c2737fffcde91b6194d0c1b4c14492
Ryan <rmfarrell@gmail.com>
2019-05-24 15:05:30 -0400

    add fototer text to i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,73 @@
+// TODO: convet to  yaml and read from storeon
 export default {
+  title: {
+    en: 'Transatlantic Periscope'
+  },
   header: {
     menu: {
       en: 'menu'
     }
   },
-  title: {
-    en: 'Transatlantic Periscope'
-  },
   view: {
     en: 'View'
+  },
+  footer: {
+    questions: {
+      en: 'Questions about the Trendspotting Platform?'
+    },
+    let_us_know: {
+      en: 'Let us Know'
+    },
+    the_site: {
+      en: 'The Site'
+    },
+    about: {
+      en: 'About'
+    },
+    guide: {
+      en: 'Guide'
+    },
+    methodology: {
+      en: 'Methodology'
+    },
+    faqs: {
+      en: 'Faqs'
+    },
+    resources: {
+      en: 'Resources'
+    },
+    insights: {
+      en: 'Insights'
+    },
+    thought_leaders: {
+      en: 'Thought Leaders'
+    },
+    our_partners: {
+      en: 'Our Partners'
+    },
+    connect: {
+      en: 'Connect'
+    },
+    news: {
+      en: 'News'
+    },
+    contact: {
+      en: 'Contact'
+    },
+    stay_informed: {
+      en: 'Stay informed and up to date:'
+    },
+    email_placeholder: {
+      en: 'Enter Email Address'
+    },
+    share: {
+      en: 'Share:'
+    },
+    privacy_policy: {
+      en: 'Privacy Policy'
+    },
+    terms: {
+      en: 'Terms of Use'
+    }
   }
 };
